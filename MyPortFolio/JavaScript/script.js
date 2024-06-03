6c8bdac91419f8c83f5ccf570a19a974e1369666
LucaMimmo <mimmoluca22@gmail.com>
2024-06-03 20:54:52 +0200

    aggiornamento javascript

--- MyPortFolio/JavaScript/script.js
+++ MyPortFolio/JavaScript/script.js
@@ -1,75 +1,53 @@
-/* document.addEventListener("DOMContentLoaded", function() {
-    window.addEventListener("scroll", function() {
-        var img = document.getElementById("dashpc1");
-        var distanceFromTop = img.getBoundingClientRect().top;
-        var windowHeight = window.innerHeight;
-        if (distanceFromTop < windowHeight * 0.5) {
-            setTimeout(function() {
-                img.style.transform = "translate(-100%, -50%)";
-                img.style.left = "50%";
-                setTimeout(function() {
-                    var dashtext = document.getElementById("dashtext");
-                    dashtext.style.display = "block"; // Mostra il testo
-                    setTimeout(function() {
-                        dashtext.classList.add("show"); // Aggiungi la classe .show per mostrare il testo con animazione
-                        dashtext.style.left = "55%";
-                    }, 100); // Ritardo per l'aggiunta della classe
-                }, 0); // Ritardo per l'animazione del testo
-            }, 0); // Ritardo per l'animazione dell'immagine
-            window.removeEventListener("scroll", arguments.callee);
-        }
-    });            
+document.getElementById('downloadButton').addEventListener('click', function() {
+    const link = document.createElement('a');
+    link.href = 'files/CV.zip';  // URL del file sul server
+    link.download = 'CV.zip';   // Nome del file da scaricare
+    link.click();
 });
 
-
-
-
-function gorules(){
-    location.replace("./tradingrules.html");
-}
-
-
-
-
-
-
-var animationInProgress = false; // Variabile di stato per tracciare se l'animazione è in corso
-
-document.addEventListener("scroll", function() {
-    var img = document.getElementById("section2");
-    var distanceFromTop = img.getBoundingClientRect().top;
-    var windowHeight = window.innerHeight;
-    if (distanceFromTop < windowHeight * 0.5 && !animationInProgress) {
-        var images = document.querySelectorAll('.image-container img');
-        var delay = 100;
-
-        function showImage(index) {
-            if (index >= images.length) {
-                animationInProgress = false; // L'animazione è terminata, reimposta la variabile di stato
-                return;
-            }
-            images[index].style.display = 'block';
-            if (index < images.length - 1) {
-                setTimeout(function() {
-                    images[index].style.display = 'none';
-                    showImage(index + 1);
-                }, delay);
-            }
-        }
-
-        animationInProgress = true; // L'animazione è in corso
-        showImage(0);
-    }
-});
-
-
-
-
- */
 
 document.getElementById('downloadButton').addEventListener('click', function() {
     const link = document.createElement('a');
     link.href = 'files/CV.zip';  // URL del file sul server
     link.download = 'CV.zip';   // Nome del file da scaricare
     link.click();
+});
+
+window.addEventListener("scroll", function() {
+    var skillsSection = document.querySelector("#skills");
+    var aboutSection = document.querySelector("#about");
+    var skillsSectionPosition = skillsSection.getBoundingClientRect();
+    var aboutSectionPosition = aboutSection.getBoundingClientRect();
+    var windowHeight = window.innerHeight;
+
+    var circularChartsSkills = skillsSection.querySelectorAll(".circular-chart");
+    var circularChartsAbout = aboutSection.querySelectorAll(".circular-chart");
+
+    circularChartsSkills.forEach(function(chart) {
+        if (
+            skillsSectionPosition.top < windowHeight &&
+            skillsSectionPosition.bottom >= 0 &&
+            chart.getBoundingClientRect().top < windowHeight
+        ) {
+            if (!chart.classList.contains("animate")) {
+                chart.classList.add("animate");
+            }
+        } else {
+            chart.classList.remove("animate");
+        }
+    });
+
+    circularChartsAbout.forEach(function(chart) {
+        if (
+            aboutSectionPosition.top < windowHeight &&
+            aboutSectionPosition.bottom >= 0 &&
+            chart.getBoundingClientRect().top < windowHeight
+        ) {
+            if (!chart.classList.contains("animate")) {
+                chart.classList.add("animate");
+            }
+        } else {
+            chart.classList.remove("animate");
+        }
+    });
 });
